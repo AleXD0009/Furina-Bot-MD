8aa232cef10c4300506182518fc701fcebb6b773
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-02 00:16:23 -0300

    ©Furina-Bot-MD🔹

--- plugins/_MID-GB.js
+++ plugins/_MID-GB.js
@@ -2,7 +2,7 @@ import { es, en } from '../lib/multi-language/_default.js'
 export async function before(m,{ conn }) {
 let idioma  = global.db.data.users[m.sender].midLanguage
 let MID_GB
-  
+
 if (idioma == "es") {
 MID_GB = es
 } else if (idioma == "en") {
