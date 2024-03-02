4846f20985cd6bfbadd8e22d52dbcf5f3b8e82c0
Ale <145930757+AleXD0009@users.noreply.github.com>
2024-03-02 00:35:26 -0300

    ©Furina-Bot-MD🔹

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -5,16 +5,16 @@ import fbDownloader from 'fb-downloader-scrapper';
 import {facebook} from '@xct007/frieren-scraper'; 
 import axios from 'axios';
 const handler = async (m, {conn, args, command, usedPrefix}) => {
-  if (!args[0]) throw `[📚] 𝑰𝒏𝒈𝒓𝒆𝒔𝒂 𝒖𝒏 𝒆𝒏𝒍𝒂𝒄𝒆 𝒅𝒆 𝒇𝒂𝒄𝒆𝒃𝒐𝒐𝒌\n\n*[💡] 𝑬𝒋𝒆𝒎𝒑𝒍𝒐:*: ${usedPrefix + command}* https://fb.watch/fOTpgn6UFQ/`;
-  if (!args[0].match(/www.facebook.com|fb.watch/g)) throw `[📚] 𝑰𝒏𝒈𝒓𝒆𝒔𝒂 𝒖𝒏 𝒆𝒏𝒍𝒂𝒄𝒆 𝒅𝒆 𝒇𝒂𝒄𝒆𝒃𝒐𝒐𝒌\n\n*[💡] 𝑬𝒋𝒆𝒎𝒑𝒍𝒐:* ${usedPrefix + command}* https://fb.watch/fOTpgn6UFQ/`;
+  if (!args[0]) throw `Ingresa un enlace de facebook\nEjemplo: *${usedPrefix + command}* https://fb.watch/fOTpgn6UFQ/`;
+  if (!args[0].match(/www.facebook.com|fb.watch/g)) throw `Ingresa un enlace de facebook\nEjemplo: *${usedPrefix + command}* https://fb.watch/fOTpgn6UFQ/`;
   try {
-    await m.reply(`*⌛ ᴅᴇsᴄᴀʀɢᴀɴᴅᴏ sᴜ ᴠɪᴅᴇᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ ᴘᴏʀ ғᴀᴠᴏʀ, ᴇsᴛᴇ ᴘʀᴏᴄᴇsᴏ ᴘᴜᴇᴅᴇ ᴅᴜʀᴀʀ ᴇɴᴛʀᴇ 2 ʏ 10 ᴍɪɴᴜᴛᴏs ᴅᴇᴘᴇɴᴅɪᴇɴᴅᴏ ᴅᴇ ʟᴀ ᴅᴜʀᴀᴄɪᴏɴ ᴅᴇʟ ᴠɪᴅᴇᴏ...*`);
+    await m.reply(`*⌛ Descargando video, esto puede tardar de 2 a 10 minutos depende la duracion del video*`);
     const d2ata = await facebook.v1(args[0]);
     let r2es = '';
     if (d2ata.urls && d2ata.urls.length > 0) {
       r2es = `${d2ata.urls[0]?.hd || d2ata.urls[1]?.sd || ''}`;
     }
-    conn.sendFile(m.chat, r2es, 'error.mp4', `𝑨𝒒𝒖𝒊 𝒕𝒊𝒆𝒏𝒆𝒔 𝒕𝒖 𝒗𝒊𝒅𝒆𝒐\n\n𝑱𝒐𝒕𝒄𝒉𝒖𝒂-𝑴𝒊𝒏𝒊`, m);
+    conn.sendFile(m.chat, r2es, 'error.mp4', `Aqui tienes tu video`, m);
   } catch (err1) {
     try {
       const req = await igeh(args[0]);
@@ -25,31 +25,31 @@ const handler = async (m, {conn, args, command, usedPrefix}) => {
         const Jjson = await Rres.json();
         let VIDEO = Jjson.result[0];
         if (VIDEO == '' || !VIDEO || VIDEO == null) VIDEO = Jjson.result[1];
-        conn.sendFile(m.chat, VIDEO, 'error.mp4', `𝑨𝒒𝒖𝒊 𝒕𝒊𝒆𝒏𝒆𝒔 𝒕𝒖 𝒗𝒊𝒅𝒆𝒐\n\n𝑱𝒐𝒕𝒄𝒉𝒖𝒂-𝑴𝒊𝒏𝒊`, m);
+        conn.sendFile(m.chat, VIDEO, 'error.mp4', `Aqui tienes tu video`, m);
       } catch (err2) {
         try {
           const ress = await fg.fbdl(args[0]);
           const urll = await ress.data[0].url;
-          await conn.sendFile(m.chat, urll, 'error.mp4', '𝑨𝒒𝒖𝒊 𝒕𝒊𝒆𝒏𝒆𝒔 𝒕𝒖 𝒗𝒊𝒅𝒆𝒐\n\n𝑱𝒐𝒕𝒄𝒉𝒖𝒂-𝑴𝒊𝒏𝒊', m);
+          await conn.sendFile(m.chat, urll, 'error.mp4', 'Aqui tienes tu video', m);
         } catch (err3) {
           try {
             const res = await fbDownloader(args[0]);
             for (const result of res.download) {
               const ur = result.url;
-              await conn.sendFile(m.chat, ur, 'error.mp4', '𝑨𝒒𝒖𝒊 𝒕𝒊𝒆𝒏𝒆𝒔 𝒕𝒖 𝒗𝒊𝒅𝒆𝒐\n\n𝑱𝒐𝒕𝒄𝒉𝒖𝒂-𝑴𝒊𝒏𝒊', m);
+              await conn.sendFile(m.chat, ur, 'error.mp4', 'Aqui tienes tu video', m);
             }
           } catch (err4) {
             try {
               const res3 = await fetch(`https://latam-api.vercel.app/api/facebookdl?apikey=nekosmic&q=${args[0]}`);
               const json = await res3.json();
               const url3 = await json.video;
-              await conn.sendFile(m.chat, url3, 'error.mp4', '𝑨𝒒𝒖𝒊 𝒕𝒊𝒆𝒏𝒆𝒔 𝒕𝒖 𝒗𝒊𝒅𝒆𝒐\n\n𝑱𝒐𝒕𝒄𝒉𝒖𝒂-𝑴𝒊𝒏𝒊', m);
+              await conn.sendFile(m.chat, url3, 'error.mp4', 'Aqui tienes 🔹', m);
             } catch (err5) {
               try {
                 const {result} = await facebookdl(args[0]).catch(async (_) => await facebookdlv2(args[0])).catch(async (_) => await savefrom(args[0]));
-                for (const {url, isVideo} of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, '*𝖠𝗊𝗎𝗂 𝖤𝗌𝗍𝖺 𝖲𝗎 𝖵𝗂𝖽𝖾𝗈*', m);
+                for (const {url, isVideo} of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, 'Aqui tienes', m);
               } catch (err6) {
-                throw `𝑬𝒓𝒓𝒐𝒓, 𝒗𝒖𝒆𝒍𝒗𝒆 𝒂 𝒊𝒏𝒕𝒆𝒏𝒕𝒂𝒓𝒍𝒐`;
+                throw `ocurrio un error, vuelve a intentarlo`;
               }
             }
           }
